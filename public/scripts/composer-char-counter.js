e1edb6a815b470314dae73568fc96b6c7abaf918
BBrandford11 <brandfordbryce@gmail.com>
2022-03-01 18:18:31 -0700

    adds hover to tweet box and removes console.logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,40 @@
 $(document).ready(function () {
-  console.log("first");
-
-  const $input = $(".input");
-  const $counter = $('counter1')
-
-  $(".new-tweet").keydown(() => {
-    console.log("keypress");    
-    const inputVal = $input.val();
-    const reamining = 140 - $('.input').val().length
-    $('.counter1').text(reamining)
-    if (reamining < 0) {
-      $('.counter1').css('color', '#ff0000');
-  }
-    
-  });
   
- 
+  const $input = $(".input");
+  // const $counter = $('counter1')
+
+  $(".new-tweet").keydown(() => {    
+    const inputVal = $input.val();
+    const reamining = 140 - $(".input").val().length;
+    $(".counter1").text(reamining);
+    if (reamining < 0) {
+      $(".counter1").css("color", "#ff0000");
+    }
+  });
+
   $(".button-71").on("click", function () {
     event.preventDefault();
     const inputVal = $input.val();
-    console.log(inputVal.length)
-    $input.val('').focus();
+    $input.val("").focus();
   });
- 
+
+  $(".displayTweet")
+    .mouseover(function () {
+      console.log("2")
+      $(this).css("box-shadow", "10px 10px 5px #888");
+    })
+    .mouseleave(function () {
+      $(this).css("box-shadow", "0px 0px 0px #888");
+    });
+
+  $(".fa-solid fa-retweet")
+    .mouseover(function () {
+      console.log("first2222")
+      $(this).css("color", "#ff0000");
+    })
+    .mouseleave(function () {
+      $(this).css("color", "#ffffff");
+    });
 
   // --- our code goes here ---
 });
