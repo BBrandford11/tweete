56895b103595af340d8c5593eebc2598c1de6d23
BBrandford11 <brandfordbryce@gmail.com>
2022-03-02 18:17:00 -0700

    adds tweets from database and adds errors for empty/ over 140 inputs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,29 @@
 $(document).ready(function () {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1469088,
-    },
-  ];
+  
+  // const data = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac",
+  //     },
+  //     content: {
+  //       text: "If I have seen further it is by standing on the shoulders of giants",
+  //     },
+  //     created_at: 1461116232227,
+  //   },
+  //   {
+  //     user: {
+  //       name: "Descartes",
+  //       avatars: "https://i.imgur.com/nlhLi3I.png",
+  //       handle: "@rd",
+  //     },
+  //     content: {
+  //       text: "Je pense , donc je suis",
+  //     },
+  //     created_at: 1469088,
+  //   },
+  // ];
 
   const renderTweets = function (tweets) {
     for (let t in tweets) {
@@ -31,9 +31,9 @@ $(document).ready(function () {
       $("#tweetContainer").append(type);
     }
   };
-
+  
   const createTweetElement = function (data1) {
-    const $tweet = `<article class="displayTweet"> <header> 
+    const $tweet = `<article class="displayTweet"> <header>      
     <p><img src="${data1.user.avatars}" /> ${data1.user.name}</p>
     <p>${data1.user.handle}</p>
     </header>
@@ -51,5 +51,23 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      method: "GET",
+      
+    })
+      .then(function (data) {
+        console.log("Passed2", data);        
+        renderTweets(data)
+      })
+      .catch(function (error) {
+        console.log("error2", error);
+      });
+  }
+
+
+  
+  loadTweets()
+  
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-  
   const $input = $(".input");
   // const $counter = $('counter1')
 
-  $(".new-tweet").keydown(() => {    
+  $(".new-tweet").keydown(() => {
     const inputVal = $input.val();
     const reamining = 140 - $(".input").val().length;
     $(".counter1").text(reamining);
@@ -12,21 +11,45 @@ $(document).ready(function () {
     }
   });
 
-  $(".button-71").on("click", function () {
+  // $(".button-71").on("click", function () {
+  //   event.preventDefault();
+  //   const inputVal = $input.val();
+  //   $input.val("").focus();
+  // });
+
+  $("form").submit(function (event) {
     event.preventDefault();
-    const inputVal = $input.val();
+    if ($(".input").val() === "" || $(".input").val() === null) {
+      alert("Please enter a tweet!");
+      return false;
+    }
+    if ($(".input").val().length > 140) {
+      alert("Tweets can only be 140 characters long!");
+      return false;
+    }
+    
+    let serialzie = $(".input").serialize();
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: serialzie,
+    })
+      .then(function (data) {
+        console.log("Passed", data);
+      })
+      .catch(function (error) {
+        console.log("error", error);
+      });
     $input.val("").focus();
   });
 
   $(".displayTweet")
     .mouseover(function () {
-      console.log("2")
       $(this).css("box-shadow", "10px 10px 5px #888");
     })
     .mouseleave(function () {
       $(this).css("box-shadow", "0px 0px 0px #888");
     });
 
-  
   // --- our code goes here ---
 });
